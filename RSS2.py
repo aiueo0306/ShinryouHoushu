cec6d971f22a6cebaa7d953ca51f56e926b92b51
aiueo0306 <sato.sota@create-sd.co.jp>
2026-06-28 17:45:16 +0900

    Update RSS2.py

--- RSS2.py
+++ RSS2.py
@@ -31,7 +31,7 @@ BASE_URL = "https://www.mhlw.go.jp/stf/newpage_67729.html"
 GAKKAI = "令和８年度診療報酬改定について"
 
 SELECTOR_TITLE = 'xpath=//*[@id="content"]/div[2]/div/div/div[15] a'
-title_selector = "a"
+title_selector = ""
 title_index = 0
 href_selector = "a"
 href_index = 0
